abae75893c1fc7a93f2779e5d50a7db7f101b7ae
volhaf <Volha.f@outlook.com>
2024-03-07 22:34:34 +0100

    feat: added adaptive title for section skills

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 import { theme } from "../styles/Theme";
+import { font } from "../styles/Common";
 
 export const SectionTitle =styled.h2 `
+    ${font({family:"'Josefin Sans', sans-serif", weight: 600, Fmax: 36, Fmin: 30})}
+
+
     color: ${theme.color.accentSecondary};
     text-align:center;
-    font-family: 'Josefin Sans', sans-serif;
-    font-weight: 600;
+    /* font-family: 'Josefin Sans', sans-serif;
+    font-weight: 600; */
     font-size: 36px;
     letter-spacing: 5px;
     margin-bottom: 90px;
@@ -22,7 +26,13 @@ export const SectionTitle =styled.h2 `
 
         position: absolute;
         left: 50%;
-        bottom: -20px;
+        bottom: -30px;
         transform: translateX(-50%);
         }
+
+        @media ${theme.media.mobile} {
+            bottom: -24px;
+        }
+
+
 `
--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -43,6 +43,10 @@ button {
 
 section {
     padding: 100px 0; 
+
+    @media ${theme.media.mobile} {
+        padding: 80px 0;
+    }
 }
 
 section:nth-of-type(odd) {
