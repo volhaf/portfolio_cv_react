f1f7565933900a39cae651735ced684f830ad848
volhaf <Volha.f@outlook.com>
2024-03-07 17:23:14 +0100

    feat: added adaptive blok main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Conteiner } from '../../../components/Conteiner';
 import { theme } from '../../../styles/Theme';
+import { font } from '../../../styles/Common';
+
 
 
 
@@ -12,7 +14,7 @@ export const Main = () => {
     return (
         <StyledMain>
            <Conteiner>
-                <FlexWrapper align={'center'} justify={'space-between'}>
+                <FlexWrapper align={'center'} justify={'space-around'} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Volha Filanovich</span></Name>
@@ -28,12 +30,6 @@ export const Main = () => {
 };
 
 
-const Photo = styled.img`
-    width: 300px;
-    height: 400px;
-    object-fit: cover;
-`
-
 const StyledMain = styled.section`
     min-height: 100vh;
     background-color: #ddc7cf;
@@ -45,6 +41,7 @@ const StyledMain = styled.section`
 const PhotoWrapper = styled.div`
     position: relative; 
     z-index: 0; 
+    margin-top: 50px;
 
     &::before {
         content: "";
@@ -56,26 +53,46 @@ const PhotoWrapper = styled.div`
         top: -24px;
         left: 24px;
         z-index: -1; 
+
+        @media ${theme.media.mobile} {
+            width: 275px;
+            height: 400px;
+            top: -17px;
+            left: 20px;
+
+        }
     }
+
 `
+
+const Photo = styled.img`
+    width: 300px;
+    height: 400px;
+    object-fit: cover;
+    margin-right: 20px;
+
+    @media ${theme.media.mobile} {
+        width: 270px;
+        height: 350px;
+
+    }
+` 
 
 
 const MainTitle = styled.h1 `
-    font-weight: 400;
-    font-size: 27px;
+${font({family:"'Josefin Sans' sans-serif", weight: 400, Fmax: 27, Fmin: 20,})}
 `  
 const Name = styled.h2 `
-    font-family: 'Josefin Sans' sans-serif;
-    font-weight: 700;
-    font-size: 50px;
+    ${font({family:"'Josefin Sans' sans-serif", weight: 700, Fmax: 50, Fmin: 36,})}
     letter-spacing: 0.05em;
-    /* margin: 10px 0; */
-    
+    margin: 10px 0;
 
 
     span {
         position: relative; 
         z-index: 0; 
+        white-space: nowrap;
+
 
         &::before {
             content: "";
@@ -89,6 +106,10 @@ const Name = styled.h2 `
             z-index: -1; 
 
         }
+    }
+
+    @media ${theme.media.mobile} {
+        margin: 15px 0 22px;
     }
 
 `  
