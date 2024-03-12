88ddc2b0d146b899e74527e2e1c55e9a96a82b98
volhaf <Volha.f@outlook.com>
2024-03-12 15:17:44 +0100

    refactor: refactoring section main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,122 +1,26 @@
 import React from 'react';
 import photo from '../../../assets/images/avatar.webp'
-import styled from 'styled-components';
 import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Conteiner } from '../../../components/Conteiner';
-import { theme } from '../../../styles/Theme';
-import { font } from '../../../styles/Common';
+import { S } from './Main_Styles'
 
 
 
-
-
-export const Main = () => {
+export const Main: React.FC = () => {
     return (
-        <StyledMain>
-           <Conteiner>
+        <S.Main>
+            <Conteiner>
                 <FlexWrapper align={'center'} justify={'space-around'} wrap={"wrap"}>
                     <div>
-                        <SmallText>Hi There</SmallText>
-                        <Name>I am <span>Volha Filanovich</span></Name>
-                        <MainTitle>Frontend Developer</MainTitle>
+                        <S.SmallText>Hi There</S.SmallText>
+                        <S.Name>I am <span>Volha Filanovich</span></S.Name>
+                        <S.MainTitle>Frontend Developer</S.MainTitle>
                     </div>
-                    <PhotoWrapper>
-                        <Photo src={photo} alt=""/>
-                    </PhotoWrapper>
+                    <S.PhotoWrapper>
+                        <S.Photo src={photo} alt=""/>
+                    </S.PhotoWrapper>
                 </FlexWrapper>
-           </Conteiner>
-        </StyledMain>
+            </Conteiner>
+        </S.Main>
     );
 };
-
-
-const StyledMain = styled.section`
-    min-height: 100vh;
-    background-color: #ddc7cf;
-    display: flex;
-    color: ${theme.color.fontColorTitle};
-
-`
-
-const PhotoWrapper = styled.div`
-    position: relative; 
-    z-index: 0; 
-
-    &::before {
-        content: "";
-        width: 300px;
-        height: 450px;
-        border: 5px solid ${theme.color.accentColor};
-
-        position: absolute;
-        top: -24px;
-        left: 24px;
-        z-index: -1; 
-
-        @media ${theme.media.mobile} {
-            width: 275px;
-            height: 400px;
-            top: -17px;
-            left: 20px;
-
-        }
-    }
-    @media ${theme.media.mobile} {
-        margin-top: 50px;
-    }
-
-`
-
-const Photo = styled.img`
-    width: 300px;
-    height: 400px;
-    object-fit: cover;
-    margin-right: 20px;
-
-    @media ${theme.media.mobile} {
-        width: 270px;
-        height: 350px;
-
-    }
-` 
-
-
-const MainTitle = styled.h1 `
-${font({family:"'Josefin Sans' sans-serif", weight: 400, Fmax: 27, Fmin: 20,})}
-`  
-const Name = styled.h2 `
-    ${font({family:"'Josefin Sans' sans-serif", weight: 700, Fmax: 50, Fmin: 36,})}
-    letter-spacing: 0.05em;
-    margin: 10px 0;
-
-
-    span {
-        position: relative; 
-        z-index: 0; 
-        white-space: nowrap;
-
-
-        &::before {
-            content: "";
-            display: inline-block; 
-            width: 100%;
-            height: 20px; 
-            background-color: ${theme.color.accentColor}; 
-
-            position: absolute;
-            bottom: 5px; 
-            z-index: -1; 
-
-        }
-    }
-
-    @media ${theme.media.mobile} {
-        margin: 15px 0 22px;
-    }
-
-`  
-
-const SmallText = styled.h2 `
-    font-weight: 400;
-    font-size: 14px;
-`  
