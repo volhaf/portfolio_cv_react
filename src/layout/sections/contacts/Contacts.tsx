ae9f75516fe55631828cd2a4ababd09bb4b5b007
volhaf <Volha.f@outlook.com>
2024-03-13 11:26:43 +0100

    refactor: refactoring section contacts

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,68 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Button } from '../../../components/button/Button';
 import { Conteiner } from '../../../components/Conteiner';
-import { theme } from '../../../styles/Theme';
+import { S } from './Contacts_Styles';
 
-export const Contacts = () => {
+
+export const Contacts: React.FC = () => {
     return (
-        <StyledContacts>
+        <S.Contacts>
             <Conteiner>
                 <SectionTitle>Contact</SectionTitle>
-                <StyledForm>
-                    <Field placeholder={'name'}/>
-                    <Field placeholder={'subject'}/>
-                    <Field as={'textarea'} placeholder={'message'}/>
+                <S.Form>
+                    <S.Field placeholder={'name'}/>
+                    <S.Field placeholder={'subject'}/>
+                    <S.Field as={'textarea'} placeholder={'message'}/>
                     <Button type={'submit'}>Send message</Button>
-                </StyledForm>
+                </S.Form>
             </Conteiner>
-        </StyledContacts>
+        </S.Contacts>
     );
 };
 
-const StyledContacts = styled.section `
-
-`
-
-const StyledForm = styled.form `
-    max-width: 540px;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 16px;
-    margin: 0 auto;
-
-
-textarea {
-    resize: none;
-    height: 155px; 
-}
-
-`
-
-const Field = styled.input `
-    width: 100%;
-    background-color: ${theme.color.secondaryBg};
-    border: 1px solid ${theme.color.fontColorTitle};
-    border-radius: 5px; 
-    padding: 7px 15px;
-
-
-    font-family: 'Poppins', sans-serif;
-    font-weight: 400;
-    font-size: 12px;
-    letter-spacing: 0.05rem;
-    color: ${theme.color.fontColorTitle};
-
-    &::placeholder {
-        color: ${theme.color.fontColorTitle};
-        text-transform: capitalize; 
-    }
-
-    &:focus-visible {
-        outline: 1px solid ${theme.color.fontColorTitle}; 
-    }
-
-`
