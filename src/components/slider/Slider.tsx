f9f790df865c3044c7009ba513dfdebab399c5ae
volhaf <Volha.f@outlook.com>
2024-03-13 12:42:43 +0100

    refactor: refactoring slider components

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,77 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
 import { FlexWrapper } from '../FlexWrapper';
-import { theme } from '../../styles/Theme';
+import {S} from './Slider_Styles'; 
 
-export const Slider = () => {
+export const Slider: React.FC = () => {
     return (
-        <StyledSlider>
+        <S.Slider>
             <FlexWrapper>
-                <Slide>
-                    <Text>
+                <S.Slide>
+                    <S.Text>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    </Text>
-                    <Name>@volha.dev</Name>
-                </Slide>
+                    </S.Text>
+                    <S.Name>@volha.dev</S.Name>
+                </S.Slide>
             </FlexWrapper>
-            <Pagination>
+            <S.Pagination>
                 <span></span>
                 <span className={"active"}></span>
                 <span></span>
-            </Pagination>
-        </StyledSlider>
+            </S.Pagination>
+        </S.Slider>
     );
 };
-
-
-const StyledSlider = styled.div`
-    max-width: 500px;
-    display: flex;
-    flex-direction: column; 
-    align-items: center; 
-`
-
-const Slide =styled.div`
-text-align: center; 
-    
-`
-
-const Text =styled.p`
-    
-`
-const Name =styled.span`
-    font-family: 'Josefin Sans', sans-serif;
-    font-weight: 600;
-    font-size: 16px;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    color: ${theme.color.fontColorTitle};
-    margin: 20px 0 40px;
-    display: inline-block; 
-`
-
-const Pagination =styled.span`
-    span {
-        display: inline-block;
-        width: 7px;
-        height: 7px; 
-        margin: 5px;
-        border-radius: 5px;
-        background-color: ${theme.color.opasityBg};
-        border-radius: 20px; 
-
-
-        & + span {
-            margin-left: 5px;
-        }
-
-        &.active {
-            background-color: ${theme.color.accentColor};
-            width: 20px;
-
-        }
-
-
-
-    }
-`
---
+++ src/components/slider/Slider_Styles.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components"
+import { theme } from "../../styles/Theme"
+
+
+
+const Slider = styled.div`
+    max-width: 500px;
+    display: flex;
+    flex-direction: column; 
+    align-items: center; 
+`
+const Slide =styled.div`
+text-align: center; 
+`
+const Text =styled.p``
+
+const Name =styled.span`
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    color: ${theme.color.fontColorTitle};
+    margin: 20px 0 40px;
+    display: inline-block; 
+`
+const Pagination =styled.span`
+    span {
+        display: inline-block;
+        width: 7px;
+        height: 7px; 
+        margin: 5px;
+        border-radius: 5px;
+        background-color: ${theme.color.opasityBg};
+        border-radius: 20px; 
+
+
+        & + span {
+            margin-left: 5px;
+        }
+
+        &.active {
+            background-color: ${theme.color.accentColor};
+            width: 20px;
+
+        }
+
+
+
+    }
+`
+
+export const S = {
+    Slider,
+    Slide,
+    Text,
+    Name,
+    Pagination,
+}
