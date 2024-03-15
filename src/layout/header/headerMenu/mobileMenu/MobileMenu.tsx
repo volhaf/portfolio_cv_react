938b8de7b7e840b2f4139fe68e3b68c1019ea7f2
volhaf <Volha.f@outlook.com>
2024-03-15 12:44:22 +0100

    feat: dynamic behavior for burger-menu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,16 +1,18 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import {S} from './../HeaderMenu_Styles'
 import { Menu } from '../menu/Menu';
 
 
 export const MobileMenu: React.FC<{menuItems: Array<string>}> = (props: {menuItems: Array<string>}) => {
+    const [menuIsOpen, setmenuIsOpen] = useState(false)
+    const onBurgerBtnClick = () => {setmenuIsOpen(!menuIsOpen)}
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={true}>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={true}>
+            <S.MobileMenuPopup isOpen={menuIsOpen} onClick={() => {setmenuIsOpen(false)}}>
                 <Menu menuItems={props.menuItems}/>
             </S.MobileMenuPopup>
         </S.MobileMenu>
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -4,7 +4,7 @@ export const theme = {
     color: { 
         primaryBg: '#f2f2ee',
         secondaryBg: '#d8d8d2',
-        opasityBg: 'rgba(96, 107, 132, 0.8)',
+        opasityBg: 'rgba(96, 107, 132, 0.9)',
 
         accentColor: '#f59f66',
         accentSecondary: '#606b84',
