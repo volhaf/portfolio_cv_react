53311d368fe0b835c834576219d43ca589ed9f56
volhaf <Volha.f@outlook.com>
2024-03-22 15:56:32 +0100

    feat: added links for footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -5,9 +5,9 @@ import { S } from './Footer_Styles';
 
 
 const socialItemData = [
-    {iconId: 'inst'},
-    {iconId: 'linkedin'},
-    {iconId: 'github'},
+    {iconId: 'inst', href: 'https://www.instagram.com/volha.dev/'},
+    {iconId: 'linkedin', href: 'www.linkedin.com/in/volha-filanovich-355a562b1'},
+    {iconId: 'github', href: 'https://github.com/volhaf'},
 ]
 
 export const Footer:React.FC = () => {
@@ -16,15 +16,13 @@ export const Footer:React.FC = () => {
             <FlexWrapper direction={'column'} align={'center'}>
             <S.Name>@volha.dev</S.Name>
                 <S.SocialList>
-
                     {socialItemData.map((s, index)=> {
                         return  <S.SocialItem key ={index}>
-                                    <S.SocialLink>
+                                    <S.SocialLink href={s.href} target="_blank">
                                         <Icon iconId ={s.iconId} height={'21'} width={'21'} viewBox={'0 0 21 21'} />
                                     </S.SocialLink>
                                 </S.SocialItem>
                     })}
-                    
                 </S.SocialList>
             <S.Copyright>© 2024 Volha Filanovich, All Rights Reserved.</S.Copyright>
             </FlexWrapper>
